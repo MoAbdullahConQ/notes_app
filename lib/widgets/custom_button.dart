5752cd5c869b5ec15f10b1ac163ce2ec5f859792
Mhmed <noorelsayd193@gmail.com>
2024-07-16 02:05:09 +0300

    finish notes bottom sheet

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 50,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: kPrimaryColor,
+      ),
+      child: const Center(
+          child: Text(
+        'Add',
+        style: TextStyle(color: Colors.black, fontSize: 20),
+      )),
+    );
+  }
+}
