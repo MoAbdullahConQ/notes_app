f00e69310cb26bbaa0256ffc2876bbd2ddde6d8f
Mhmed <noorelsayd193@gmail.com>
2024-07-15 03:22:34 +0300

    show model buttom sheet

--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -8,14 +8,18 @@ class NotesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      physics: const BouncingScrollPhysics(),
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 4.0),
-          child: NoteItem(),
-        );
-      },
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: ListView.builder(
+        padding: EdgeInsets.zero,
+        physics: const BouncingScrollPhysics(),
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+            child: NoteItem(),
+          );
+        },
+      ),
     );
   }
 }
